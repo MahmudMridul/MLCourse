e914f7fc640f4b65c66628310f051d1ea080c87a
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-01-02 18:38:12 +0600

    Add scikit-learn

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -1,7 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+print(x)
